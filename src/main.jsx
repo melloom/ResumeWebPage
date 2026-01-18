07dc87ffd37b848b3244952e55fb5a251e76d68c
Melvin <Melvin.a.p.cruz@gmail.com>
2026-01-18 17:07:09 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './assets/styles/global.css';
-import { registerServiceWorker, unregisterAllServiceWorkers } from './pwaRegistration';
+import { unregisterAllServiceWorkers } from './pwaRegistration';
 
-// Check if we need to reset service worker (for emergency fixes)
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get('resetSW') === 'true') {
-  unregisterAllServiceWorkers().then(() => {
-    // Remove the query parameter and reload
-    window.history.replaceState({}, document.title, window.location.pathname);
-    window.location.reload();
-  });
-} else {
-  // Register service worker for PWA support
-  registerServiceWorker();
-}
+// TEMPORARILY DISABLE SERVICE WORKER TO TEST
+unregisterAllServiceWorkers().then(() => {
+  console.log('Service worker disabled for testing');
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
